e3645727206a0fa1e364314f08e5d564105f2375
弓厶 <lph279430114@qq.com>
2019-06-15 11:34:59 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("test")
+    public JavaBean test() {
+        JavaBean bena = new JavaBean();
+        bena.setId(System.currentTimeMillis());
+        bena.setName("Hello");
+        return bena;
+    }
+
+    public class JavaBean {
+        private long id;
+        private String name;
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
